29edc94143255c824eef4dff099592fdd36af159
Tim Schneider <timphillip.schneider@t-online.de>
2022-07-01 14:53:28 +0200

    details page

--- cocktails/routes.py
+++ cocktails/routes.py
@@ -6,3 +6,8 @@ from cocktails import app, all_cocktails
 def main():
     return render_template("index.html", cocktails=all_cocktails)
 
+@app.route("/cocktail/<id>/")
+def detailed_view(id=0):
+    print(all_cocktails[int(id)])
+    return render_template("cocktail.html", cocktail=all_cocktails[int(id)])
+
